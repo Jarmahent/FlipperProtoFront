9e8a2038f9a396ea14a9c53e0d410d44a1e119e8
Kevin Hernandez <kevin.hernandez@risingtide.us>
2025-06-24 22:20:57 -0400

    Initial boiler plate

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1 style="color:red">Vue 3 Boilerplate</h1>
+    <HelloWorld msg="Welcome to Vue 3!" />
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="hello">
+        <input v-model="msg" placeholder="Enter a message" />
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('')
+</script>
+
+<style scoped>
+.hello {
+    font-size: 1.2rem;
+}
+</style>
